dbccb33a2178b0775e5722cfcd48bc049163d3a9
Enofua Divine <divineenofua@gmail.com>
2022-09-16 12:46:39 -1200

    latest commit

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
  const close = document.getElementById('close');
  const open = document.getElementById('open');
  const modal = document.getElementById('modal');
+ const openbar = document.getElementById('openbar')
+//  const closebar = document.getElementById('closebar')
+
 
  //toggle nav 
  toggle.addEventListener('click', () => {
-    document.body.classList.toggle('.show-nav')
- })
+    document.body.classList.toggle('show-nav');
+    openbar.classList.toggle('uil-times');
+    
+ });
+//  show modal
+open.addEventListener('click', () => {
+   modal.classList.add('show-modal');
+});
+// hide modal
+close.addEventListener('click', () => {
+   modal.classList.remove('show-modal');
+}); 
+
+// hide modal on outside click
+window.addEventListener('click', e => {
+   e.target == modal ? modal.classList.remove('show-modal') : false
+});
